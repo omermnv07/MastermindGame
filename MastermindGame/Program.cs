96d86561ab9e27aebe716a972d1464ad04352a61
omermnv07 <52194216+omermnv07@users.noreply.github.com>
2020-09-24 08:06:54 +0300

    Add files via upload

---
+++ MastermindGame/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace MastermindGame
+{
+    class Program
+    {
+        static void Main()
+        {
+            // TODO: Eger birden fazla oyun ekleyecek olursak, implementasyonu degistirip DI eklememiz grekiyor .
+            IGame game = new MastermindGame();
+            game.Start();
+            game.Play();
+            game.End();
+        }
+    }
+}
